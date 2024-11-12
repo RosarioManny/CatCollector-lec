170836594f10ced43e4d72745e2b31a006e6431c
RosarioManny <rosario.emm47@gmail.com>
2024-11-12 12:00:21 -0500

    Added SuperUser, Create Catlist + CatDetail path

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Home
+from .views import Home, CatList, CatDetail
 
 urlpatterns = [
-    path("",Home.as_view(), name="Home" )
+    path("", Home.as_view(), name="Home" ),
+    path("cats/", CatList.as_view(), name="Cat-list"),
+    path("cats/<int:id>/", CatDetail.as_view(), name="Cat-detail"),
 ]
